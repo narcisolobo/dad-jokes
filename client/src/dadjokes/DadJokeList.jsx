de62a6929249dea217c6b36e393d7ae86aefc34a
narcisolobo <narcisolobo@gmail.com>
2023-04-24 15:30:55 -0700

    subtitle on list now conditional

--- client/src/dadjokes/DadJokeList.jsx
+++ client/src/dadjokes/DadJokeList.jsx
@@ -1,14 +1,21 @@
-import { Fragment, useEffect, useState } from 'react';
+/* React */
+import { Fragment, useContext, useEffect, useState } from 'react';
+
+/* react-router */
 import { useOutletContext } from 'react-router-dom';
 
+/* local */
+import { AuthContext } from '../context/AuthContext';
 import DadJokeItem from './DadJokeItem';
-
 import styles from '../css/dadjoke-list.module.css';
 
 function DadJokeList() {
   const { getAllJokes } = useOutletContext();
   const [jokes, setJokes] = useState([]);
   const [isCurrent, setIsCurrent] = useState(false);
+  const {
+    state: { user },
+  } = useContext(AuthContext);
 
   useEffect(() => {
     getAllJokes()
@@ -19,12 +26,16 @@ function DadJokeList() {
       .catch((err) => console.log(err));
   }, [isCurrent]);
 
+  let subtitle = 'Login or register to roll your eyes at these horrible jokes.';
+
+  if (user) {
+    subtitle = 'Click the button to roll your eyes at a joke!';
+  }
+
   return (
     <Fragment>
       <h1>All Dad Jokes</h1>
-      <h5 className="mb-4">
-        Login or register to roll your eyes at these horrible jokes.
-      </h5>
+      <h5 className="mb-4">{subtitle}</h5>
       {isCurrent && (
         <div className={styles.grid}>
           {jokes.map((joke) => (
